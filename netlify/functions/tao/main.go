35d2ce7dd96d45e76e7e13d5fdb925aa04a9a05f
Billy Z <bzajac@videoamp.com>
2023-01-31 11:20:04 -0800

    sprintf for href

--- netlify/functions/tao/main.go
+++ netlify/functions/tao/main.go
@@ -28,7 +28,7 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 	// Remember to add 1 here because the slice starts with position 0
 	tetragram := tetragrams[randInt.Int64()]
 	tetragram_num := randInt.Int64() + 1
-        href := "<a href='https://terebess.hu/english/tao/Wing.html#Kap%02d'>"
+        href := fmt.Sprintf("<a href='https://terebess.hu/english/tao/Wing.html#Kap%02d'>", tetragram_num)
 	link_tet := fmt.Sprintf("%s%s</a>", href, tetragram)
 	link_num := fmt.Sprintf("%s%0d</a>", href, tetragram_num)
 	body := fmt.Sprintf("<html><body><h1>%s</h1>\n<h2>%s</h2>\n%s\n</body></html>", link_tet, link_num, t)
